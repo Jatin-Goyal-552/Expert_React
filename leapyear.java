c5c8173056651a4bf3295920087aa6c02eee476d
Dinesh Kumar Meena <86797375+dineshmeena98@users.noreply.github.com>
2021-10-23 12:45:53 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,35 @@
+public class leapyear
+{
+
+    public static void main(String[] args)
+     {
+    
+        int year=2019;
+        if(year%4==0)
+        {
+
+            System.out.println("leap year");
+
+            if(year%100==0)
+            {
+                System.out.println("not a year");
+                if(year%400==0)
+                {
+                    System.out.println("leap year");
+                }
+                else 
+                {
+                    System.out.println("not a leap year");
+                }
+            }
+            else
+            {
+                System.out.println("leap year");
+            }
+        }
+        else
+        {
+        System.out.println("not a leap year");
+        }
+    }
+}
